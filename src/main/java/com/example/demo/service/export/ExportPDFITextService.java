e06d10ca4621615630007bbe10b83d20a4082b9e
Pcharlet <charletpierre6@gmail.com>
2018-04-17 12:26:44 +0200

    export pdf

--- src/main/java/com/example/demo/service/export/ExportPDFITextService.java
+++ src/main/java/com/example/demo/service/export/ExportPDFITextService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.export;
 
+import com.example.demo.dto.ClientDTO;
 import com.example.demo.dto.FactureDTO;
+import com.example.demo.dto.LigneFactureDTO;
 import com.example.demo.entity.Facture;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -18,6 +20,16 @@ public class ExportPDFITextService {
         Document document = new Document();
         PdfWriter.getInstance(document, os);
         document.open();
+        
+        document.addTitle("Facture");
+        document.add(new Paragraph (facture.getClient().getNom()));
+        document.add(new Paragraph (facture.getClient().getPrenom()));
+        document.add(new Paragraph ("factures"));
+        
+        for (LigneFactureDTO factu : facture.getLigneFactures() ) {
+        	document.add(new Paragraph (factu.getDesignation()));
+        	document.add(new Paragraph (factu.getQuantite()));
+        }
 
         document.add(new Paragraph("Hello"));
         document.close();
