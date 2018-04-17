b697ec810b957559f518af30e9dcc6d4f5371d5e
Pcharlet <charletpierre6@gmail.com>
2018-04-17 12:08:50 +0200

    fait mais problème affichage des 2 clients)

--- src/main/java/com/example/demo/service/export/ExportXLSXService.java
+++ src/main/java/com/example/demo/service/export/ExportXLSXService.java
@@ -1,6 +1,10 @@
 package com.example.demo.service.export;
 
 import com.example.demo.dto.ClientDTO;
+import com.lowagie.text.Row;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,27 @@ public class ExportXLSXService {
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("clients");
 
+        XSSFRow headerRow = sheet.createRow(0);
+        XSSFRow headerRow1 = sheet.createRow(1);
+        XSSFRow headerRow2 = sheet.createRow(2);
+        
+        XSSFCell cellNom = headerRow.createCell(0);
+        XSSFCell cellPrenom = headerRow.createCell(1);
+        
+        XSSFCell cellnom1 = headerRow1.createCell(0);
+        XSSFCell cellprenom1 = headerRow1.createCell(1);
+        
+        cellNom.setCellValue("Nom");
+        cellPrenom.setCellValue("Prenom");
+        
+        StringBuilder str = new StringBuilder();
+        
+        for (ClientDTO client : clients ) {
+        	cellnom1.setCellValue(client.getNom());
+        	cellprenom1.setCellValue(client.getPrenom());
+        }
+        
+        		
         workbook.write(os);
         workbook.close();
     }
